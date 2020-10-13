46e1aa4d433ba33550013ba83965e3cfdda66bf0
Guillou33 <autiergilles@gmail.com>
2020-10-13 16:49:08 +0200

    chat-app components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import Contact from './components/Contact';
+
 function App() {
   return (
     <div className='App'>
-      <Contact />
+      <Contact
+        id='1'
+        name='Anita Sutton'
+        avatar='https://randomuser.me/api/portraits/women/75.jpg'
+        online
+      />
+      <Contact
+        id='2'
+        name='Jim Georges'
+        avatar='https://randomuser.me/api/portraits/men/97.jpg'
+      />
+      <Contact
+        id='3'
+        name='Charlotte Ryan'
+        avatar='https://randomuser.me/api/portraits/women/20.jpg'
+        online
+      />
     </div>
   );
 }
